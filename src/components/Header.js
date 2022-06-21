1ce3fd3648b68484ac0f32a363f584cc098d8ed4
rousyati <82667388+rousyati@users.noreply.github.com>
2022-06-21 21:11:07 +0700

    Done
    Done Kak

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <h1>Call a Friend</h1>
+      <p>your friendly contact app</p>
+      <hr />
+    </>
+  );
+};
 
 export default Header;
